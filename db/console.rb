6a91d51636357677a167d47981f17c961b3b818a
Tony Tang <teokigi@hotmail.com>
2020-03-25 19:49:27 +0000

    able to create and delete albums

--- db/console.rb
+++ db/console.rb
@@ -1,61 +1,64 @@
 require_relative('../models/artist.rb')
 require_relative('../models/album.rb')
 
-artist01 = Artist.new({'name'=>'Michael Jackson'})
-artist02 = Artist.new({'name'=>'Weird Al Yankovic'})
-artist03 = Artist.new({'name'=>'Bon Jovi'})
-artist04 = Artist.new({'name'=>'Miyavi'})
-artist05 = Artist.new({'name'=>'Nobuo Uematsu'})
 
-album01 = Album.new({   'title'=>'Dangerous',
-                        'genre'=>'Rock n Pop',
-                        'artist_id'=> artist01.id})
-album02 = Album.new({   'title'=>'Thriller',
-                        'genre'=>'Rock n Pop',
-                        'artist_id'=> artist01.id})
-album03 = Album.new({   'title'=>'Mandatory Fun',
-                        'genre'=>'Parody',
-                        'artist_id'=> artist02.id})
-album04 = Album.new({   'title'=>'Running With Scissors',
-                        'genre'=> 'Parody',
-                        'artist_id'=> artist02.id})
-album05 = Album.new({   'title'=>'Slippery When Wet',
-                        'genre'=> 'Rock',
-                        'artist_id'=> artist03.id})
-album06 = Album.new({   'title'=>'Keep The Faith',
-                        'genre'=> 'Rock',
-                        'artist_id'=> artist03.id})
-album07 = Album.new({   'title'=>'Firebird',
-                        'genre'=>'J Rock',
-                        'artist_id'=> artist04.id})
-album08 = Album.new({   'title'=>'Miyavism',
-                        'genre'=>'J Rock',
-                        'artist_id'=> artist04.id})
-album09 = Album.new({   'title'=>'Music of Final Fantasy III',
-                        'genre'=>'Game',
-                        'artist_id'=>artist05.id})
-album10 = Album.new({   'title'=>'Final Fantasy VI Soundtrack',
-                        'genre'=>'game',
-                        'artist_id'=> artist05.id})
+
+
 
 answer = ""
 while answer != "exit"
         p "delete all table entries? yes/no"
         answer = gets.chomp
         if answer == "yes"
+                Album.delete_all
                 Artist.delete_all
                 p "cleared all artists"
                 answer = ""
         end
-        
-        p "populate artists? yes.no"
+
+        p "populate artists&albums? yes.no"
         answer = gets.chomp
         if answer == "yes"
+                artist01 = Artist.new({'name'=>'Michael Jackson'})
+                artist02 = Artist.new({'name'=>'Weird Al Yankovic'})
+                artist03 = Artist.new({'name'=>'Bon Jovi'})
+                artist04 = Artist.new({'name'=>'Miyavi'})
+                artist05 = Artist.new({'name'=>'Nobuo Uematsu'})
                 artist01.save
                 artist02.save
                 artist03.save
                 artist04.save
                 artist05.save
+                album01 = Album.new({   'title'=>'Dangerous',
+                                        'genre'=>'Rock n Pop',
+                                        'artist_id'=> artist01.id})
+                album02 = Album.new({   'title'=>'Thriller',
+                                        'genre'=>'Rock n Pop',
+                                        'artist_id'=> artist01.id})
+                album03 = Album.new({   'title'=>'Mandatory Fun',
+                                        'genre'=>'Parody',
+                                        'artist_id'=> artist02.id})
+                album04 = Album.new({   'title'=>'Running With Scissors',
+                                        'genre'=> 'Parody',
+                                        'artist_id'=> artist02.id})
+                album05 = Album.new({   'title'=>'Slippery When Wet',
+                                        'genre'=> 'Rock',
+                                        'artist_id'=> artist03.id})
+                album06 = Album.new({   'title'=>'Keep The Faith',
+                                        'genre'=> 'Rock',
+                                        'artist_id'=> artist03.id})
+                album07 = Album.new({   'title'=>'Firebird',
+                                        'genre'=>'J Rock',
+                                        'artist_id'=> artist04.id})
+                album08 = Album.new({   'title'=>'Miyavism',
+                                        'genre'=>'J Rock',
+                                        'artist_id'=> artist04.id})
+                album09 = Album.new({   'title'=>'Music of Final Fantasy III',
+                                        'genre'=>'Game',
+                                        'artist_id'=>artist05.id})
+                album10 = Album.new({   'title'=>'Final Fantasy VI Soundtrack',
+                                        'genre'=>'game',
+                                        'artist_id'=> artist05.id})
                 album01.save
                 album02.save
                 album03.save
@@ -81,6 +84,18 @@ while answer != "exit"
                 p "there are currently no arists in this table"
                 end
         end
+        p "list all albums? yes/no"
+        answer = gets.chomp
+        if answer == "yes"
+                albums = Album.read_all
+                if albums != nil
+                        for album in albums
+                            p "id:#{album.id}| Album title:#{album.title}| genre:#{album.genre}|artist_id:#{album.artist_id}"
+                        end
+                else
+                p "there are currently no albums in this table"
+                end
+        end
 
         p "look for artist by id? yes/no"
         answer = gets.chomp
@@ -89,12 +104,12 @@ while answer != "exit"
                 answer = gets.chomp
                 summary = Artist.read_by_id(answer)
                 if summary != nil
-                p "id:#{summary.id}| Artist: #{summary.name}"
-                answer = ""
-            else
-                p "Error:id##{answer} doesn't exist"
-                answer = ""
-            end
+                        p "id:#{summary.id}| Artist: #{summary.name}"
+                        answer = ""
+                else
+                        p "Error:id##{answer} doesn't exist"
+                        answer = ""
+                end
         end
 
         p "update artist by id? yes/no"
@@ -109,16 +124,16 @@ while answer != "exit"
                 p "artist updated"
         end
 
-    p "delete an artist?"
-    answer = gets.chomp
-    if answer == "yes"
-        p "enter artist id you wish to delete"
+        p "delete an artist?"
         answer = gets.chomp
-        Artist.delete_by_id(answer)
-        p "artist has been removed"
-    end
+        if answer == "yes"
+                p "enter artist id you wish to delete"
+                answer = gets.chomp
+                Artist.delete_by_id(answer)
+                p "artist has been removed"
+        end
 
 
-    p "type 'exit' to quit, or hit return to repeat"
-    answer = gets.chomp
+        p "type 'exit' to quit, or hit return to repeat"
+        answer = gets.chomp
 end
--- models/album.rb
+++ models/album.rb
@@ -8,16 +8,27 @@ class Album
     def initialize(options)
         @title = options['title']
         @genre = options['genre']
-        @artist_id = options['artist_id']
+        @artist_id = options['artist_id'].to_i
         @id = options['id'].to_i if options['id']
 
     end
 
+    def self.delete_all
+        sql = "DELETE FROM albums"
+        SqlRunner.run(sql,[])
+    end
     def save
         sql = "INSERT INTO albums (title,genre,artist_id)
             VALUES ($1,$2,$3) RETURNING *"
         values = [@title,@genre,@artist_id]
         @id = SqlRunner.run(sql,values)[0]['id'].to_i
+    end
+
+    def self.read_all
+        sql = "SELECT * FROM albums"
+        summary_hash = SqlRunner.run(sql,[])
+        return nil if summary_hash.first() == nil
+        return summary_hash.map{|album|Album.new(album)}
     end
 
 end
