393db555a86d4500c8201101052acab01934dbd0
Tony Tang <teokigi@hotmail.com>
2020-03-25 16:35:25 +0000

    created artist entries, wrote save method for artist, testing save

---
+++ db/console.rb
@@ -0,0 +1,23 @@
+require_relative('../models/artist.rb')
+require_relative('../models/album.rb')
+
+artist01 = Artist.new{'name'=>'Michael Jackson'}
+artist02 = Artist.new{'name'=>'Weird Al Yankovic'}
+artist03 = Artist.new{'name'=>'Bon Jovi'}
+artist04 = Artist.new{'name'=>'Miyavi'}
+artist05 = Artist.new{'name'=>'Nobuo Uematsu'}
+
+#Artist.Delete_all
+#Artist.save
+artist01.save
+artist02.save
+artist03.save
+artist04.save
+artist05.save
+#artist.read_all
+
+#artist.read_by_id
+
+#Artist.update_by_id
+
+#artist.delete_by_id
---
+++ models/artist.rb
@@ -0,0 +1,17 @@
+require_relative('../db/sql_runner.rb')
+
+class Artist
+
+    attr_accessor :name
+    attr_reader :id
+
+    def initialize(options)
+        @name = options['name']
+        @id = options['id'].to_i if options['id']
+    end
+
+    def save
+        sql = "INSERT INTO artists (name) VALUES ($1) RETURNING *"
+        values = [@name]
+        @id = SqlRunner.run(sql,values)[0]['id'].to_i
+    end
